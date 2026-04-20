4bbadd29d41b95b5fcc18908f29fa0e1880de955
Tejas Jagtap <jagtaptejas9923@gmail.com>
2026-04-20 19:27:42 +0530

    Create Rowcsv file.py

---
+++ Rowcsv file.py
@@ -0,0 +1,11 @@
+import csv
+
+# Open the CSV file
+with open("data.csv", "r") as file:
+    reader = csv.reader(file)
+    
+    row_count = 0
+    for row in reader:
+        row_count += 1
+
+print("Total number of rows:", row_count)
